bf822c4336d232a6f633c8c927708dcacd88e8a9
wissal <wissal@example.com>
2025-10-20 22:02:31 +0200

    Mise à jour du site

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Accessor Phone' );
+define( 'DB_NAME', 'accessor phone' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
